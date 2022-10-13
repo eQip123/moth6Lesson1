777d3d73309c95056d44bd73e29e0f7d129dd651
eQip <aidarasanakunov@gmail.com>
2022-10-13 19:37:41 +0600

    init App

---
+++ exampleGit/exampleGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  exampleGit
+//
+//  Created by Aidar Asanakunov on 13/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
