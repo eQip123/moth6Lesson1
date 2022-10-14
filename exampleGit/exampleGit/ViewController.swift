2caa6a8611f91e4322cc67f9a858c2dc03e7f2c8
eQip <aidarasanakunov@gmail.com>
2022-10-14 13:25:01 +0600

    test

--- exampleGit/exampleGit/ViewController.swift
+++ exampleGit/exampleGit/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     var name: String = "Hello"
+    var name2: String = "Hello"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
